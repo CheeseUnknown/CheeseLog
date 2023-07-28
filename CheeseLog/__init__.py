2ef437b6c00ef0dcb10da59a13f6d86e3288edc0
cheese <cheese@cheese.ren>
2023-07-29 01:10:02 +0800

    取消了timerTemplate，合并至messageTemplate
    添加print检测，避免不必要的print开销

--- CheeseLog/__init__.py
+++ CheeseLog/__init__.py
@@ -1,12 +1,13 @@
 import os, sys, threading, queue, datetime
 from typing import Optional
+from io import TextIOWrapper
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 from CheeseType import NonNegativeInt
 
 class Level:
-    def __init__(self, weight: NonNegativeInt, color: Optional[str] = None, messageTemplate: Optional[str] = None, timerTemplate: Optional[str] = None):
+    def __init__(self, weight: NonNegativeInt, color: Optional[str] = None, messageTemplate: Optional[str] = None):
         '''
         ### Args
 
@@ -15,31 +16,27 @@ class Level:
         - color：控制台打印的等级标签样式。
 
         - messageTemplate：消息格式，默认为`logger.messageTemplate`。
-
-        - timerTemplate：日期格式，默认为`logger.timerTemplate`。
         '''
 
         self.weight: NonNegativeInt = weight
         self.color: Optional[str] = color
         self.messageTemplate: Optional[str] = messageTemplate
-        self.timerTemplate: Optional[str] = timerTemplate
 
 class Logger(threading.Thread):
     def __init__(self):
         self.filePath: Optional[str] = None
-        self.messageTemplate: str = '(%level) %timer > %content'
-        self.timerTemplate: str = '%Y-%m-%d %H:%M:%S.%f'
+        self.messageTemplate: str = '(%level) %Y-%m-%d %H:%M:%S.%f > %content'
         self.filter: NonNegativeInt | set[str] = set()
         self.levels: dict[str, Level] = {
-            'DEBUG': Level(10, '34', None, None),
-            'INFO': Level(20, '32', None, None),
-            'STARTING': Level(20, '32', None, None),
-            'ENDING': Level(20, '34', None, None),
-            'HTTP': Level(20, None, None, None),
-            'WEBSOCKET': Level(20, None, None, None),
-            'WARNING': Level(30, '33', None, None),
-            'DANGER': Level(40, '31', None, None),
-            'ERROR': Level(50, '35', None, None)
+            'DEBUG': Level(10, '34', None),
+            'INFO': Level(20, '32', None),
+            'STARTING': Level(20, '32', None),
+            'ENDING': Level(20, '34', None),
+            'HTTP': Level(20, None, None),
+            'WEBSOCKET': Level(20, None, None),
+            'WARNING': Level(30, '33', None),
+            'DANGER': Level(40, '31', None),
+            'ERROR': Level(50, '35', None)
         }
         self.colorful: bool = True
         self._queue: queue.Queue = queue.Queue()
@@ -51,14 +48,11 @@ class Logger(threading.Thread):
         self._flag = True
         while self._flag or not self._queue.empty():
             level, now, message = self._queue.get()
-
-            message = (logger.levels[level].messageTemplate or logger.messageTemplate).replace('%level', level).replace('%timer', now.strftime(logger.levels[level].timerTemplate or logger.timerTemplate)).replace('%content', message).replace('\n', '\n    ') + '\n'
+            message = ''.join([ now.strftime((self.levels[level].messageTemplate or self.messageTemplate).replace('%level', level).replace('%content', message).replace('\n', '\n    ')), '\n' ])
             if self.filePath is not None:
                 os.makedirs(os.path.dirname(self.filePath), exist_ok = True)
-                with open(self.filePath, 'a', encoding = 'utf-8') as f:
-                    f.write(message)
-            else:
-                self._flag = False
+            with open(self.filePath, 'a', encoding = 'utf-8') as f:
+                f.write(message)
 
     def stop(self):
         self._flag = False
@@ -82,14 +76,12 @@ def default(level: str, message: str, colorfulMessage: Optional[str] = None, log
     ''' Terminal '''
     now = datetime.datetime.now()
     message = f'{message}'
-    if logger.colorful:
-        if colorfulMessage is None:
-            terminalMessage = (logger.levels[level].messageTemplate or logger.messageTemplate).replace('%level', f'\033[{logger.levels[level].color}m{level}\033[0m' if logger.levels[level].color else level).replace('%timer', f'\033[2m{now.strftime(logger.levels[level].timerTemplate or logger.timerTemplate)}\033[0m').replace('%content', message).replace('\n', '\n    ')
+    if sys.stdout.isatty():
+        if logger.colorful:
+            terminalMessage = now.strftime((logger.levels[level].messageTemplate or logger.messageTemplate).replace('%level', f'\033[{logger.levels[level].color}m{level}\033[0m' if logger.levels[level].color else level).replace('%content', colorfulMessage or message).replace('\n', '\n    '))
         else:
-            terminalMessage = (logger.levels[level].messageTemplate or logger.messageTemplate).replace('%level', f'\033[{logger.levels[level].color}m{level}\033[0m' if logger.levels[level].color else level).replace('%timer', f'\033[2m{now.strftime(logger.levels[level].timerTemplate or logger.timerTemplate)}\033[0m').replace('%content', colorfulMessage).replace('\n', '\n    ')
-    else:
-        terminalMessage = (logger.levels[level].messageTemplate or logger.messageTemplate).replace('%level', level).replace('%timer', now.strftime(logger.levels[level].timerTemplate or logger.timerTemplate)).replace('%content', message).replace('\n', '\n    ')
-    print(terminalMessage)
+            terminalMessage = now.strftime((logger.levels[level].messageTemplate or logger.messageTemplate).replace('%level', level).replace('%content', message).replace('\n', '\n    '))
+        print(terminalMessage)
 
     ''' Log file writter '''
     if logger.filePath is not None:
